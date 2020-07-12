776460734bd1373e3615a2c187556fc3c6046d03
Kai <kai.kuehl@outlook.de>
2020-07-12 20:22:55 +0200

    1.1

--- .idea/src/Docents/NeuesModul.js
+++ .idea/src/Docents/NeuesModul.js
@@ -1,3 +1,35 @@
+var mysql = require('mysql');
+
+function getConnection() {
+    let connection = mysql.createConnection({
+        host: "localhost",
+        user: "testuser",
+        password: "testuser",
+        database: "testdb",
+    });
+    return connection;
+}
+/**
+ *
+ * @param id user to change data
+ * @param columnName to update
+ * @param entryChange new information
+ */
+function insertNewModul(name, beginn,ende){
+    let con = getConnection();
+    con.connect(function(err) {
+        if (err) throw err;
+        var sqlStatment = "INSERT INTO modul (Name, Beginn, Ende) VALUES (" +
+            Modul.name + ", " +
+            Modul.beginn + " " +
+            Modul.ende + ";";
+        con.query(sqlStatment, function (err, result) {
+            if (err) throw err;
+            console.log(result.affectedRows + " record(s) updated");
+        });
+    });
+}
+
 function newModul() {
 
     class Modul {
@@ -16,5 +48,8 @@ function newModul() {
         document.getElementById(modulBeginn).value,
         document.getElementById(modulEnde).value
     );
+    insertNewModul();
+
+
 
 }
